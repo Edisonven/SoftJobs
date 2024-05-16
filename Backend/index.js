b8f795eb807a462e6046b004a15121218fae7a76
Edison <aalevenegass@gmail.com>
2024-05-16 09:28:28 -0400

    Creando archivos para estructura de datos MVC

--- Backend/index.js
+++ Backend/index.js
@@ -13,19 +13,19 @@ app.use(json());
 const verifyToken = (req, res, next) => {
   const autorizacionHeader = req.headers.authorization;
   if (!autorizacionHeader) {
-    return res.status(401).json({ message: "token no válido@" });
+    return res.status(401).json({ message: "token inválido" });
   }
 
   const [bearer, token] = autorizacionHeader.split(" ");
 
   if (bearer !== "Bearer" || !token) {
-    return res.status(401).json({ message: "token no válido hola profe tito" });
+    return res.status(401).json({ message: "Autorización inválida" });
   }
   try {
     jwt.verify(token, process.env.JWT_SECRET) && next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ message: "token no válido!!!" });
+    return res.status(401).json({ message: "token no válido, intenta nuevamente" });
   }
 };
 
