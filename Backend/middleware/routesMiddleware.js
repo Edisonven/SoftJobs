653fb4626ca39024cc72bf96596fda2565cd03f6
Edison <aalevenegass@gmail.com>
2024-05-16 14:14:59 -0400

    Actualizando middleware de rutas

--- Backend/middleware/routesMiddleware.js
+++ Backend/middleware/routesMiddleware.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.PGHOST || "localhost";
 
 export const checkRoutes = (req, res, next) => {
   const route = req.url;
   const user = req.body.email;
   console.log(
-    `Consulta recibida a la siguiente ruta :http://localhost:${PORT}${route} , usuario:`,
+    `Consulta recibida a la siguiente ruta :http://${HOST}:${PORT}${route} , usuario:`,
     user
   );
   next();
